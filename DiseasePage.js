19157c896abf17e7e915df4849c1a353c060d5fb
Dawit <dawitymersha@gmail.com>
2022-12-03 15:07:29 -0500

    search button added

--- DiseasePage.js
+++ DiseasePage.js
@@ -52,54 +52,5 @@ for(var i = 0 ;i<=inventory.length;i++){
 var searcharea = document.getElementById('search_area').value;
 
 
-function search() {
-	for (var i = 0; i <=inventory.length ; i++) {
-		if(event.key === 'Enter' && document.getElementById('search_area').value.toLowerCase() == inventory[i]["name"] ) {
-			var header = document.getElementById('heading');
-			var disc = document.getElementById('discription');
-			var image = document.getElementById('diseases_img');
-			var paragraph = document.getElementById('Not');
-			var paragraph2 = document.getElementById('Note');
-			var linkpage = document.getElementById('link_page');
 
-
-
-			linkpage.style.visibility="visible";
-			linkpage.innerHTML=("Click here for more information regarding: " + inventory[i]["name"].toLocaleUpperCase());
-			linkpage.href = (inventory[i]["Link"]);
-
-
-			paragraph2.style.visibility="visible";
-			paragraph.style.visibility ="hidden";
-			image.style.visibility = "visible";
-			header.style.visibility ="visible";
-			disc.style.visibility="visible";
-
-			header.innerHTML = (inventory[i]["name"].toUpperCase());
-			disc.innerHTML =(inventory[i]["quantity"]);
-			image.src=(inventory[i]["Image"]);
-			paragraph2.innerHTML = (inventory[i]["discription"])
-			event.preventDefault();
-			break;
-		} 
-		else if(event.key === 'Enter' && i==inventory.length-1){
-			var header = document.getElementById('heading');
-			var disc = document.getElementById('discription');
-			var image = document.getElementById('diseases_img');
-			var paragraph = document.getElementById('Not');
-			var paragraph2 = document.getElementById('Note');
-			var linkpage = document.getElementById('link_page');
-
-
-			linkpage.style.visibility="hidden";
-			paragraph2.style.visibility="hidden";
-			paragraph.style.visibility="visible";
-			header.style.visibility ="hidden";
-			image.style.visibility="hidden";
-			disc.style.visibility="hidden";
-			paragraph.innerHTML = "Nothing Matchs Your Search Sorry";
-			event.preventDefault();
-		}
-	}
-}
 
